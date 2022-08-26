841840d152519d82665eb7d8723aeb5fabcdf07a
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-26 15:46:03 +0300

    Scan from standard input and variable types

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	// define variable/const and print to standard output
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application!")
-	fmt.Println("We have total of", conferenceTickets,
-		"tickets and", remainingTickets, "are still available.")
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n",
+		conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
+
+	// taking user input
+	var firstName string
+	var lastName string
+	var email string
+	var tickets uint
+
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter your email address:")
+	fmt.Scan(&email)
+	fmt.Println("How many tickets do you need?")
+	fmt.Scan(&tickets)
+
+	fmt.Printf("Thanks %v %v for ordering %v tickets. You will receive your tickets in %v.\n",
+		firstName, lastName, tickets, email)
 }
